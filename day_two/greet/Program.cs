c08a77c3972387eac62e204f8c95e4563f9d2c5c
Pedro Douglas Pereira Barreto <doougllas@hotmail.com.br>
2022-10-04 20:47:57 -0300

    ⚡️ feat: create new projet of console
    create 2 files and delete 4 files

---
+++ day_two/greet/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace greet
+{
+  public static class Class1
+  {
+    public static string Greet()
+    {
+      return "Hello, World!";
+    }
+    static void Main()
+    {
+
+    }
+  }
+}
